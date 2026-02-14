d1e7161f59d4006a079319c6e8f91b40f8a6aac4
Jun Seo <112793862+junseo85@users.noreply.github.com>
2026-02-14 01:24:10 -0600

    genericsExtra

---
+++ GenericsExtra/src/Main.java
@@ -0,0 +1,97 @@
+package dev.lpa;
+
+import dev.lpa.model.LPAStudent;
+import dev.lpa.model.LPAStudentComparator;
+import dev.lpa.util.QueryList;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+//    public static void main(String[] args) {
+//        int studentCount = 10;
+//        List<Student> students = new ArrayList<>();
+//        for (int i=0; i<studentCount; i++){
+//            students.add(new Student());
+//        }
+//        students.add(new LPAStudent());
+//        //printList(students);
+//        printMoreLists(students);
+//
+//        List<LPAStudent> lpaStudents = new ArrayList<>();
+//        for (int i=0; i<studentCount; i++){
+//            lpaStudents.add(new LPAStudent());
+//        }
+//        //printList(lpaStudents);
+//        printMoreLists(lpaStudents);
+//
+//        testList(new ArrayList<String>(List.of("Able", "Barry","Charlie")));
+//        testList(new ArrayList<Integer>(List.of(1,2,3)));
+//
+//        var queryList = new QueryList<>(lpaStudents);
+//        var matches = queryList.getMatches("Course", "Python");
+//        printMoreLists(matches);
+//
+//        var students2021 = QueryList.getMatches(students, "YearStarted", "2021");
+//        printMoreLists(students2021);
+public static void main(String[] args) {
+    QueryList<LPAStudent> queryList = new QueryList<>();
+    for (int i = 0; i<25; i++){
+        queryList.add(new LPAStudent());
+        }
+    System.out.println("Ordered ");
+    queryList.sort(Comparator.naturalOrder());
+    printList(queryList);
+
+    System.out.println("Matches ");
+    var matches = queryList.getMatches("PercentComplete", "50")
+            .getMatches("Course", "Python");
+    matches.sort(new LPAStudentComparator());
+    printList(matches);
+
+    System.out.println("Ordered");
+    matches.sort(null);
+    printList(matches);
+    }
+    public static void printList(List<?> students){
+    for(var student : students){
+        System.out.println(student);
+    }
+    }
+
+//    public static void printMoreLists(List<? extends Student> students){ //wildcard   ?
+//        for(var student : students){
+//            //System.out.println(student);
+//            System.out.println(student.getYearStarted() + ": "+ student);
+//        }
+//        System.out.println();
+//    }
+//
+//    public static void testList(List<?> list){
+//        for (var element: list){
+//            if (element instanceof String s){
+//                System.out.println("String: "+ s.toUpperCase());
+//            }else if (element instanceof Integer i){
+//                System.out.println("Integer: "+ i.floatValue());
+//            }
+//        }
+//    }
+//    public static void testList(List<String> list){
+//        for (var element: list){
+//            System.out.println("String: "+ element.toUpperCase());
+//        }
+//    }
+//    public static void testList(List<Integer> list){
+//        for (var element: list){
+//            System.out.println("Integer: "+ element.floatValue());
+//        }
+//    }
+
+//    public static <T extends Student>void printList(List<T> students){
+//        for(var student : students){
+//            //System.out.println(student);
+//            System.out.println(student.getYearStarted() + ": "+ student);
+//        }
+//        System.out.println();
+//    }
+}
